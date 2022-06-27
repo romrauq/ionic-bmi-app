a9e660ffc960849451f422837ba44bd082640afc
John Quarmor <johnequarmor@gmail.com>
2022-06-27 15:40:31 +0100

    - Added descriptive text to output. - Set BMI result to display with 2 decimal places.

--- src/components/BmiResult.tsx
+++ src/components/BmiResult.tsx
@@ -6,8 +6,9 @@ const BmiResult: React.FC<{ result: number }> = (props) => {
 		<IonRow>
 			<IonCol>
 				<IonCard>
-					<IonCardContent>
-						<h2>{props.result.toFixed(2)}</h2>
+					<IonCardContent className="ion-text-center">
+						<h2>Your Body Mass Index is: </h2>
+						<h3>{props.result.toFixed(2)}</h3>
 					</IonCardContent>
 				</IonCard>
 			</IonCol>
